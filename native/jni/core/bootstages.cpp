9e789895ec27b7b7582938bd10a6f00664d44995
pencc <you@example.com>
2021-06-21 15:17:22 +0800

    remove manager install in boot_complete

--- native/jni/core/bootstages.cpp
+++ native/jni/core/bootstages.cpp
@@ -352,18 +352,18 @@ void boot_complete(int client) {
 
     auto_start_magiskhide(true);
 
-    if (!check_manager()) {
-        if (access(MANAGERAPK, F_OK) == 0) {
-            // Only try to install APK when no manager is installed
-            // Magisk Manager should be upgraded by itself, not through recovery installs
-            rename(MANAGERAPK, "/data/magisk.apk");
-            install_apk("/data/magisk.apk");
-        } else {
-            // Install stub
-            auto init = MAGISKTMP + "/magiskinit";
-            exec_command_sync(init.data(), "-x", "manager", "/data/magisk.apk");
-            install_apk("/data/magisk.apk");
-        }
-    }
+    // if (!check_manager()) {
+    //     if (access(MANAGERAPK, F_OK) == 0) {
+    //         // Only try to install APK when no manager is installed
+    //         // Magisk Manager should be upgraded by itself, not through recovery installs
+    //         rename(MANAGERAPK, "/data/magisk.apk");
+    //         install_apk("/data/magisk.apk");
+    //     } else {
+    //         // Install stub
+    //         auto init = MAGISKTMP + "/magiskinit";
+    //         exec_command_sync(init.data(), "-x", "manager", "/data/magisk.apk");
+    //         install_apk("/data/magisk.apk");
+    //     }
+    // }
     unlink(MANAGERAPK);
 }
